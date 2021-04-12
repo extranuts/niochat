2ae7ca9a354488df1defff841c2f9cbf14360671
Insane <valeev.timur.v@gmail.com>
2021-04-12 16:29:36 +0300

    Initial commit

---
+++ server/src/main/java/com/server/MainHandler.java
@@ -0,0 +1,35 @@
+package com.server;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author
+ * @return
+ */
+
+
+public class MainHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Клиент подключился");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        while (byteBuf.readableBytes() > 0) {
+            System.out.print((char)byteBuf.readByte());
+        }
+        byteBuf.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
